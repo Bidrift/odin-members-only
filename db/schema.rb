91fa703b6f232c4977f46516dd0c2e165855e576
Bidrift <yassine.fighter@gmail.com>
2025-06-15 23:58:04 +0100

    Fix typo in references

--- db/schema.rb
+++ db/schema.rb
@@ -14,10 +14,10 @@ ActiveRecord::Schema[8.0].define(version: 2025_06_15_223553) do
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.string "body"
-    t.integer "users_id", null: false
+    t.integer "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["users_id"], name: "index_posts_on_users_id"
+    t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -32,5 +32,5 @@ ActiveRecord::Schema[8.0].define(version: 2025_06_15_223553) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  add_foreign_key "posts", "users", column: "users_id"
+  add_foreign_key "posts", "users"
 end
